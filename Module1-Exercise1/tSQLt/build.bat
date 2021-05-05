d13659df8b7863f035685959ac65add03f508c26
olivia.adams <olivia.adams@aimco.ca>
2021-05-05 09:40:48 -0600

    Adding changed exercise 1 and 2 files to my repo

--- Module1-Exercise1/tSQLt/build.bat
+++ Module1-Exercise1/tSQLt/build.bat
@@ -3,9 +3,8 @@ pushd "%~dp0"
 
 echo download tSQLt from https://tsqlt.org/download/tsqlt/
 if not exist "%~dp0\temp\" mkdir "%~dp0\temp\"
-curl https://tsqlt.org/download/tsqlt/ -o "%~dp0\temp\tSQLt.zip"
+REM curl https://tsqlt.org/download/tsqlt/ -o "%~dp0\temp\tSQLt.zip"
 tar -xf "%~dp0\temp\tSQLt.zip" -C "%~dp0\temp\\"
 
 "%SQLCMD_HOME%\sqlcmd.exe" -i "%~dp0\build.sql" -S "%~1" %~2 -d "%~3"
 
-popd
